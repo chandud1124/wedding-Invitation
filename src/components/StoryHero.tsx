903b5608df87b504782a17bf852a38461b6929c2
chandud1124 <chandu.d1124@gmail.com>
2026-08-16 15:14:32 +0530

    Remove unused old component files and stylesheets to clean up the repository

--- src/components/StoryHero.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const StoryHero: React.FC = () => {
-  // This section has been moved to OurStory. You can safely remove this component if not needed elsewhere.
-  return null;
-};
-
-export default StoryHero;
